498d2810d37adb34760e79e79bf558624af712ff
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-18 10:53:23 +0530

    Add files via upload

---
+++ src/day2/demo2.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class demo2 {
+	void m1() {
+		System.out.println("sdfa");
+	}
+	void m1(int a) {
+		System.out.println("dfasdfasdfasfas");
+	}
+
+	public static void main(String[] args) {
+		demo2 v=new demo2();
+		v.m1();
+		v.m1(2);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
