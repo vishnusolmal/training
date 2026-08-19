3e4bb5f97871524c3b2299a7d06be71c5aed23aa
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-19 10:48:57 +0530

    Update exc3.java

--- src/day3/exc3.java
+++ src/day3/exc3.java
@@ -1,1 +1,27 @@
+package day3;
 
+public class exc3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+		try {
+			System.out.println(10 / 2);
+			int[] a = { 10, 20, 30 };
+			System.out.println(a[0]);
+			System.out.println(a[1]);
+			System.out.println(a[2]);
+			System.out.println(a[3]);
+
+		}catch (ArithmeticException e) {
+			System.out.println(" Array handle");
+			
+	}catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(" Array handle");
+	
+	}
+		System.out.println("Hello sir");
+	}
+		// TODO Auto-generated method stub
+
+	}
