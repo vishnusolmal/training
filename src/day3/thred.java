6f598129c3855a60029c73541e2925eb11140b6d
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-19 12:51:53 +0530

    Update thred.java

--- src/day3/thred.java
+++ src/day3/thred.java
@@ -1,1 +1,20 @@
+package day3;
 
+public class thred extends Thread {
+	public void run() {
+		for(int i=0;i<5;i++) {
+			System.out.println("method"+i);
+		
+	}
+	}
+
+	public static void main(String[] args) {
+		thred bb=new thred();
+		bb.start();
+		for(int i=0;i<5;i++) {
+			System.out.println("main method"+i);
+		}
+
+	}
+
+}
