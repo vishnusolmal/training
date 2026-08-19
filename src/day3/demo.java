4cdeae83482cd79d8ea84cefbcd093fef23cce8e
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-19 15:19:42 +0530

    Update demo.java

--- src/day3/demo.java
+++ src/day3/demo.java
@@ -1,1 +1,26 @@
+package day3;
+class MyTask implements Runnable {
 
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Child Thread: " + i);
+        }
+    }
+}
+  
+public class demo {
+
+	public static void main(String[] args) {
+		  MyTask task = new MyTask();
+
+	        Thread t = new Thread(task);
+
+	        t.start();
+
+	        for (int i = 1; i <= 5; i++) {
+	            System.out.println("Main Thread: " + i);
+	        }
+
+	}
+
+}
