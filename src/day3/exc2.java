ac728a44aae61c1fe993ac6691da215fe967b149
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-19 10:48:20 +0530

    Update exc2.java

--- src/day3/exc2.java
+++ src/day3/exc2.java
@@ -1,1 +1,20 @@
+package day3;
+
+public class exc2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+		try {
+			System.out.println(10 / 0);
+
+
+		} catch (ArithmeticException e) {
+			System.out.println(" arthi handle");
+		} 
+		System.out.println("Hello sir");
+	}
+		// TODO Auto-generated method stub
+
+	}
 
