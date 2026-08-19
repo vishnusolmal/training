9aa302ea9a1fa2432ed24d370ab5875c27f1bef6
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-19 10:50:10 +0530

    Update exc4.java

--- src/day3/exc4.java
+++ src/day3/exc4.java
@@ -1,1 +1,20 @@
+package day3;
+
+public class exc4 {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("vishnu");
+			System.out.println(10/0);
+		}catch(NullPointerException ne) {
+			System.out.println("u r getting ne" + ne);
+		}finally {
+			System.out.println("finally block is always executed");
+		}
+		System.out.println("rest of the code");
+		// TODO Auto-generated method stub
+
+	}
+
+}
 
