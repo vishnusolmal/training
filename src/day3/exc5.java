67cb8c1b25e4059760ccb7d40b4b0eca7d88c9eb
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-19 10:50:36 +0530

    Update exc5.java

--- src/day3/exc5.java
+++ src/day3/exc5.java
@@ -1,1 +1,21 @@
+package day3;
+
+public class exc5 {
+
+	public static void main(String[] args) {
+		System.out.println("anand");
+		System.out.println("anand3");
+		System.exit(0);
+		
+		try {
+			int a = 10 / 0;
+			System.out.println(a);
+		} finally {
+			System.out.println("finally block");
+		}
+		System.out.println("rest of the code");
+
+	}
+
+}
 
