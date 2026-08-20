973cc95928978bf4d104632078b94db52d967e8b
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-20 16:17:18 +0530

    Create sum.java

---
+++ src/day4/sum.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class sum {
+
+	public static void main(String[] args) {
+		int salary[]= {10000,15000,12000,18000,20000};
+		int Totalsalary=0;
+		for(int i=0;i<=4;i++) {
+			Totalsalary=Totalsalary+salary[i];	
+		}
+		System.out.println("Total salary:  "+Totalsalary);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
