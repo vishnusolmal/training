69a7ee4cad41b312232afe71213707323c573711
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-20 16:29:25 +0530

    Update bill.java

--- src/day4/bill.java
+++ src/day4/bill.java
@@ -1,1 +1,16 @@
+package day4;
+
+public class bill {
+
+	public static void main(String[] args) {
+		int units[]= {10,15,12,20,18,14,16};
+		int Totalbill=0;
+		for(int i=0;i<=4;i++) {
+			Totalbill=Totalbill+units[i];	
+		}
+		System.out.println(" total electricity consumption for 7 days:  "+Totalbill);
+
+	}
+
+}
 
