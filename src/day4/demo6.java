d7198f3f6a048847d61144a647bf35882990a6ae
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-20 16:15:42 +0530

    Create demo6.java

---
+++ src/day4/demo6.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class demo6 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=20;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			} 
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
