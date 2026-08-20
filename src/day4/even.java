73679283ff2fd17938c7dbf20364c4ceba7fad5a
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-20 16:16:18 +0530

    Create even.java

---
+++ src/day4/even.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class even {
+
+	public static void main(String[] args) {
+		int num=10;
+		int remin=num%2;
+		System.out.println("reminder: "+remin);
+		if(remin==0) {
+			System.out.println("Number is even");
+		}else {
+			System.out.println("number is odd");
+		}
+		
+
+	}
+
+}
