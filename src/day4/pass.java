ea23fd06a80f82a91ff327a827fc93b2fb0a45a2
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-20 16:31:37 +0530

    Create pass.java

---
+++ src/day4/pass.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class pass {
+
+	public static void main(String[] args) {
+		int marks[]= {35, 67, 89, 22, 45, 30, 76};
+		for(int i=0;i<=6;i++) {
+			if(marks[i]>=40) {
+				System.out.println(marks[i]);
+				
+			}
+				
+		}
+		
+
+
+	}
+
+}
