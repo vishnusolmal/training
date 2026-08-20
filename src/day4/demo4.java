04f6713b1799b3709b4126cf5d3b00080cd074e3
vishnusolmal <vishnusolmal42@gmail.com>
2026-08-20 16:14:43 +0530

    Create demo4.java

---
+++ src/day4/demo4.java
@@ -0,0 +1,15 @@
+package day4;
+
+public class demo4 {
+
+	public static void main(String[] args) {
+		int students=23;
+		int studentsPerRow=5;
+		int studentsleft=students%studentsPerRow;
+		System.out.println( studentsleft);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
